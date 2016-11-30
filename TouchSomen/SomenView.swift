cb7e8d4f2a2e2a752045b31d1a90c7621ce5ae86
s.murakami <smurakami.84@gmail.com>
2016-11-30 14:01:31 +0900

    箸を実装

--- TouchSomen/SomenView.swift
+++ TouchSomen/SomenView.swift
@@ -11,6 +11,7 @@ import Cocoa
 class SomenView: NSView {
     
     var somens: [Somen] = []
+    var hashis: [Hashi] = []
     var pos = CGPoint()
     var timer: Timer? = nil
     var somenCounter = 0
@@ -30,10 +31,11 @@ class SomenView: NSView {
         }
         
         for somen in somens {
-            somen.pos.x -= 4
-            if somen.pos.x <= -80 {
-                somen.isValid = false
-            }
+            somen.update()
+        }
+        
+        for hashi in hashis {
+            hashi.update()
         }
         
         if somenCounter == 0 {
@@ -42,6 +44,7 @@ class SomenView: NSView {
         }
         
         somens = somens.filter { $0.isValid }
+        hashis = hashis.filter { $0.isValid }
         
         somenCounter -= 1
         needsDisplay = true
@@ -53,12 +56,22 @@ class SomenView: NSView {
         somens.append(somen)
     }
     
+    func addHashi(at point: NSPoint) {
+        let hashi = Hashi()
+        let size = hashi.getSize(bounds)
+        hashi.pos = point
+        hashi.pos.x -= size.width/2
+        hashi.pos.y = 0
+        hashis.append(hashi)
+    }
+    
     override func touchesBegan(with event: NSEvent) {
         if let touch = event.touches(matching: .began, in: self).first, touch.type == .direct {
             let location = touch.location(in: self)
+            addHashi(at: location)
             for somen in somens {
                 if somen.hit(point: location) {
-                    somen.isValid = false
+                    somen.remove()
                 }
             }
         }
@@ -73,6 +86,10 @@ class SomenView: NSView {
 //        }
 //        
         drawWater(dirtyRect)
+        
+        for hashi in hashis {
+            hashi.draw(dirtyRect)
+        }
         
         for somen in somens {
             somen.draw(dirtyRect)
@@ -102,19 +119,32 @@ class Somen: NSObject {
     let images = Somen.getImages()
     var imageIndex = 0
     var counter = 0
+    var removeTime = 0
+    var isRemoving = false
     
     let shadow = NSImage(named: "soumen_shadow")!
-    let hitWidth: CGFloat = 60 // 当たり判定
+    let hitWidth: CGFloat = 40.5 // 当たり判定
     var isValid = true
     
     static func getImages() -> [NSImage] {
         let imageNames = [
-            "soumen1", "soumen2", "soumen3", "soumen4", "soumen5", "soumen6", "soumen7", "soumen8", "soumen9", "soumen10",
+//            "soumen1", "soumen2", "soumen3", "soumen4", "soumen5", "soumen6", "soumen7", "soumen8", "soumen9", "soumen10",
+            "soumen_1",
+            "soumen_2",
+            "soumen_3",
+            "soumen_4",
             ]
         return imageNames.map { NSImage(named: $0)! }
     }
     
-    func hit(point: CGPoint) -> Bool {
+    func remove() {
+        removeTime = counter
+        isRemoving = true
+    }
+    
+    func hit(point point_: CGPoint) -> Bool {
+        var point = point_
+        point.x += 4 * 9
         return point.x > pos.x && point.x < pos.x + hitWidth
     }
     
@@ -128,23 +158,42 @@ class Somen: NSObject {
         return size
     }
     
+    func update() {
+        pos.x -= 4
+        
+        if isRemoving {
+            let duration = counter - removeTime
+            
+            if duration > 10 {
+                pos.y -= 4
+            }
+        }
+        
+        if pos.x <= -80 {
+            isValid = false
+        }
+        
+        if pos.y >= 80 {
+            isValid = false
+        }
+    }
+    
     func draw(_ dirtyRect: NSRect) {
         let height = dirtyRect.size.height * 0.6
-        pos.y = (dirtyRect.size.height - height) / 2
-        
+        var pos = self.pos
+        pos.y += (dirtyRect.size.height - height) / 2
         let size = getSize(dirtyRect)
         
-        imageIndex = counter
         images[imageIndex % images.count].draw(in: NSRect(
             origin: pos,
             size: size))
         
-//        if counter % 4 == 0 {
-//            imageIndex += Int(arc4random() % 2) * 2 - 1
-//            if imageIndex < 0 {
-//                imageIndex += images.count * 100
-//            }
-//        }
+        if counter % 4 == 0 {
+            imageIndex += Int(arc4random() % 2) * 2 - 1
+            if imageIndex < 0 {
+                imageIndex += images.count * 100
+            }
+        }
         counter += 1
     }
     
@@ -161,6 +210,40 @@ class Somen: NSObject {
 }
 
 class Hashi: NSObject {
+    var pos = NSPoint()
+    let images = Hashi.getImages()
+    var counter = 0
     
+    var isValid = true
+    
+    static func getImages() -> [NSImage] {
+        let imageNames = [ "hashi_100", "hashi_101", "hashi_102", "hashi_103", "hashi_104", "hashi_105", "hashi_106", "hashi_107", "hashi_108", "hashi_109", "hashi_110", "hashi_111", "hashi_112", "hashi_113", "hashi_114", "hashi_115", "hashi_116", "hashi_117", "hashi_118", ]
+        return imageNames.map { NSImage(named: $0)! }
+    }
+    
+    func update() {
+        counter += 1
+        if counter == images.count {
+            isValid = false
+        }
+    }
+    
+    func draw(_ dirtyRect: NSRect) {
+        let imageIndex = counter % images.count
+        let image = images[imageIndex]
+        
+        let size = getSize(dirtyRect)
+        image.draw(in: NSRect(origin: pos, size: size))
+    }
+    
+    func getSize(_ dirtyRect: NSRect) -> NSSize {
+        let image = images.first!
+        let height = dirtyRect.size.height * 0.9
+        let scale = height / image.size.height
+        let size = NSSize(
+            width: image.size.width * scale,
+            height: image.size.height * scale)
+        return size
+    }
 }
 
