f33f3b7f0819181855be7113f48097f73ce18f09
s.murakami <smurakami.84@gmail.com>
2016-11-30 18:23:31 +0900

    killing by window

--- TouchSomen/WindowController.swift
+++ TouchSomen/WindowController.swift
@@ -12,6 +12,7 @@ class WindowController: NSWindowController {
 
     override func windowDidLoad() {
         super.windowDidLoad()
+        self.window?.delegate = self
     
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
     }
@@ -51,3 +52,8 @@ extension WindowController: NSTouchBarDelegate {
     }
 }
 
+extension WindowController: NSWindowDelegate {
+    func windowWillClose(_ notification: Notification) {
+        NSApplication.shared().terminate(NSApp.keyWindow!)
+    }
+}
